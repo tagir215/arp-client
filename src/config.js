a2c23e2697556f7cdf35b6e1d9cb733af3ba487e
Tagir Tsarkov <117892331+tagir215@users.noreply.github.com>
2024-01-26 15:16:54 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const baseURLClient = "https://arp-client-c1df0d4c8a26.herokuapp.com";
-//export const baseURLServer = "https://arp200-f1d4c58ab3e8.herokuapp.com"
+export const baseURLServer = "https://arp200-f1d4c58ab3e8.herokuapp.com"
 //export const baseURLServer = "https://arp-server.azurewebsites.net"
-export const baseURLServer = "http://localhost:8080";
+//export const baseURLServer = "http://localhost:8080";
 
 export const icons = [
     "icon-motherboard.png",
